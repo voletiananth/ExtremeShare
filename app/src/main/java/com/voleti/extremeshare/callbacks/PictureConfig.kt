1f36abe010c6c90ff7cd1f306379edd3f5300aea
voletiananth <voletiananthkumar@gmail.com>
2020-07-06 19:45:59 +0530

    init

--- app/src/main/java/com/voleti/extremeshare/callbacks/PictureConfig.kt
+++ app/src/main/java/com/voleti/extremeshare/callbacks/PictureConfig.kt
@@ -3,17 +3,31 @@ package com.voleti.extremeshare.callbacks
 import android.content.Context
 import android.database.Cursor
 import android.net.Uri
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
-import com.voleti.extremeshare.ui.baseUI.BaseModelImpl
-import com.voleti.extremeshare.ui.baseUI.config.BaseConfig
-import com.voleti.extremeshare.ui.datamodels.Header
-import com.voleti.extremeshare.ui.datamodels.PictureContent
+import com.voleti.extremeshare.ui.baseUI.config.DynamicConfig
 import kotlinx.coroutines.CoroutineScope
 
-class PictureConfig:BaseConfig(){
-    override fun mainLayoutManager(context: Context?): RecyclerView.LayoutManager {
+class PictureConfig:DynamicConfig(){
+    override fun subLayoutManager(context: Context?): RecyclerView.LayoutManager = LinearLayoutManager(context)
+
+    override fun subViewType(position: Int): Int {
+        TODO("")
+
+    }
+
+    override fun subBind(viewDataBinding: ViewDataBinding, position: Int) {
         TODO("Not yet implemented")
+    }
+
+    override fun mainLayoutManager(context: Context?): RecyclerView.LayoutManager = GridLayoutManager(context,3).apply {
+        spanSizeLookup = object : GridLayoutManager.SpanSizeLookup(){
+            override fun getSpanSize(position: Int): Int = if (mainData[position].viewType== headerType) 1 else spanCount
+
+        }
     }
 
     override val contentUri: Uri
